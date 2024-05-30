5c3f24433b9f98779d4cef10513aab23b2aefd34
Filipe Mallmann Siota <filipe.siota@diarioescola.com.br>
2024-05-30 00:03:15 -0300

    FEAT: Add custom logger

--- cmd/storify/main.go
+++ cmd/storify/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/storify/backend/internal/router"
+import (
+	"github.com/storify/backend/configs"
+	"github.com/storify/backend/internal/router"
+)
+
+var (
+	logger *configs.Logger
+)
 
 func main () {
+	logger = configs.GetLogger("main")
+
+	err := configs.InitializeDatabase()
+	if err != nil {
+		logger.Errorf("database initialization failed: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
